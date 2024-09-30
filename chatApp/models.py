975c338e397b3ec71a8058326e8f806ce406f2f8
James <kamaujames3705@gmail.com>
2024-09-30 17:21:04 +0300

    registerd admin

--- chatApp/models.py
+++ chatApp/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TimeField()
+    message = models.TextField()
     response = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.username}: {self.message}'
+        return f'{self.user.username}: {self.message}'
